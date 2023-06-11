2822a0727cab59065113bda3ef651aa25197851e
brhnaydeniz <burhan.aydeniz0101@hotmail.com>
2023-06-11 16:46:48 +0300

    register_page_v01

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                Session["user"] = model;
+                return RedirectToAction("Index");
+            }
+            else
+            return View(model);
+        }
+        public ActionResult LogOut()
+        {
+            Session.Clear();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class RegisterViewModel
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage ="Parola Yanlış...")]
+        public string Repassword { get; set; }
+
+    }
+}
